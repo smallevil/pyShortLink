0a0e7bcf861ddb5222917e826f6f7385cbb7ffdb
smallevil@163.com <smallevil@163.com>
2020-12-04 18:34:03 +0800

    修复https被跳到http的问题

--- code/apps/__init__.py
+++ code/apps/__init__.py
@@ -2,9 +2,10 @@
 # @Author: smallevil
 # @Date:   2020-11-24 10:48:40
 # @Last Modified by:   smallevil
-# @Last Modified time: 2020-12-02 13:34:28
+# @Last Modified time: 2020-12-04 18:33:34
 
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 from .views.share import share
 from .views.admin import admin
 from .views.front import front
@@ -12,6 +13,7 @@ from .models.AdminModel import AdminModel
 import os, logging, logging.handlers
 
 app = Flask(__name__, template_folder='templates', static_folder='static', instance_relative_config=True)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)  #修复https会跳到http的问题
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 app.register_blueprint(share)
--- code/apps/views/admin.py
+++ code/apps/views/admin.py
@@ -2,9 +2,9 @@
 # @Author: smallevil
 # @Date:   2020-11-24 10:48:40
 # @Last Modified by:   smallevil
-# @Last Modified time: 2020-12-04 16:00:09
+# @Last Modified time: 2020-12-04 18:15:34
 
-from flask import Blueprint, render_template, redirect, session, request, current_app
+from flask import Blueprint, render_template, redirect, session, request, current_app, url_for
 import functools
 from ..models.AdminModel import AdminModel
 import math, urllib, arrow, json
